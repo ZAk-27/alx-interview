062d65b60c89e6b447818df46545f945393a6188
ZAk-27 <touhadi.zakaria@gmail.com>
2024-04-11 18:29:00 -0700

    up

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -6,28 +6,28 @@ import sys
 def solve_queens_problem(board_size):
     """N queens solution finder module"""
 
-    def is_valid_position(pos, occupied_pos):
+    def is_valid_position(plc, occupied_plc):
         """N queens solution finder module"""
-        for i in range(len(occupied_pos)):
+        for i in range(len(occupied_plc)):
             if (
-                occupied_pos[i] == pos or
-                occupied_pos[i] - i == pos - len(occupied_pos) or
-                occupied_pos[i] + i == pos + len(occupied_pos)
+                occupied_plc[i] == plc or
+                occupied_plc[i] - i == plc - len(occupied_plc) or
+                occupied_plc[i] + i == plc + len(occupied_plc)
             ):
                 return False
         return True
 
-    def place_queens(board_size, index, occupied_pos, solutions):
+    def place_queens(board_size, index, occupied_plc, solutions):
 """ N queens solution finder module"""
         if index == board_size:
-            solutions.append(occupied_pos[:])
+            solutions.append(occupied_plc[:])
             return
 
         for i in range(board_size):
-            if is_valid_position(i, occupied_pos):
-                occupied_pos.append(i)
-                place_queens(board_size, index + 1, occupied_pos, solutions)
-                occupied_pos.pop()
+            if is_valid_position(i, occupied_plc):
+                occupied_plc.append(i)
+                place_queens(board_size, index + 1, occupied_plc, solutions)
+                occupied_plc.pop()
 
     solutions = []
     place_queens(board_size, 0, [], solutions)
