25a4079dfc522721cb701c6807e9071337f1374e
ZAk-27 <touhadi.zakaria@gmail.com>
2024-04-19 02:46:28 -0700

    utf8 upd

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,21 +13,21 @@ def validUTF8(data):
         (True): all char in data are valid UTF-8 code point
         (False):  one or more char in data are invalid code point
     """
-    rm = 1 << 8
-    mc = 1 << 7
+    realmadrid = 1 << 8
+    mancity = 1 << 7
     byteCt = 0
-    for cl in data:
-        uefa = 1 << 7
+    for wembly in data:
+        uefaCl = 1 << 7
         if byteCt == 0:
-            while uefa & cl:
+            while uefaCl & wembly:
                 byteCt += 1
-                uefa = uefa >> 1
+                uefaCl = uefaCl >> 1
             if byteCt == 0:
                 continue
             if byteCt == 1 or byteCt > 4:
                 return False
         else:
-            if not (cl & rm and not (cl & mc)):
+            if not (wembly & realmadrid and not (wembly & mancity)):
                 return False
         byteCt -= 1
     return not byteCt
